baf06b607d57da822a6c3fcc4e5e57bf5b5be899
someHobo <xwei7778@gmail.com>
2015-01-26 20:27:25 -0800

    Made a change to cause merge conflict

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/GreetingService.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/GreetingService.java
@@ -9,8 +9,11 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("greet")
 public interface GreetingService extends RemoteService {
 	String greetServer(String name) throws IllegalArgumentException;
-
-	
-	//MADE A CHANGE TO TEST COMMIT
+/**
+ * MADE SOME CHANGES~!!!!
+ * @@@@@@@@@@@
+ * THIS SHOULD CAUSE merge conflict	!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
+//	//MADE A CHANGE TO TEST COMMIT
 
 }
