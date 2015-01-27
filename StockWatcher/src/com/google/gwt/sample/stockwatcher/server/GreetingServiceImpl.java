5101b3b5351acf7276a74a2811f906c38b1fc87d
someHobo <xwei7778@gmail.com>
2015-01-26 20:33:08 -0800

    changed greetServer method to cause merge conflict

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
@@ -12,23 +12,9 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		GreetingService {
 
 	public String greetServer(String input) throws IllegalArgumentException {
-		// Verify that the input is valid. 
-		if (!FieldVerifier.isValidName(input)) {
-			// If the input is not valid, throw an IllegalArgumentException back to
-			// the client.
-			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
-		}
-
-		String serverInfo = getServletContext().getServerInfo();
-		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-
-		// Escape data from the client to avoid cross-site script vulnerabilities.
-		input = escapeHtml(input);
-		userAgent = escapeHtml(userAgent);
-
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+		System.out.println("this should cause merge conflict");
+		
+		return "";
 	}
 
 	/**
