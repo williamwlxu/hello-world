c0fda2a4680013fd8d4cec5714affe24840f7e57
someHobo <xwei7778@gmail.com>
2015-01-26 20:42:09 -0800

    Reoworked GreetingServiceImpl to cause merge conflict

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/server/GreetingServiceImpl.java
@@ -5,30 +5,26 @@ import com.google.gwt.sample.stockwatcher.shared.FieldVerifier;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
- * The server-side implementation of the RPC service.
+ * Comment goes here
  */
 @SuppressWarnings("serial")
 public class GreetingServiceImpl extends RemoteServiceServlet implements
 		GreetingService {
+	
+	int a = 0;
+	int b = -1;
 
-	public String greetServer(String input) throws IllegalArgumentException {
-		System.out.println("this should cause merge conflict");
-		
-		return "";
+	@Override
+	public String greetServer(String name) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	/**
-	 * Escape an html string. Escaping data received from the client helps to
-	 * prevent cross-site script vulnerabilities.
-	 * 
-	 * @param html the html string to escape
-	 * @return the escaped string
-	 */
-	private String escapeHtml(String html) {
-		if (html == null) {
-			return null;
-		}
-		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
-				.replaceAll(">", "&gt;");
+	public void printStuff(){
+		;
+	}
+	
+	public void doStuff(){
+		a = 2;
 	}
 }
